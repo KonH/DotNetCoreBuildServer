bf5317e707039bd406ade63fb8afae8baa977b47
KonH <konh@yandex.ru>
2017-07-20 23:34:08 +0700

    Simple file logging;

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleClient {
 				new BuildNode("validate_sources", "check_file_exist", new Dictionary<string, string>(){{"path", "{root}/sources.txt"}}),
 				new BuildNode("clean_project", "delete_file", new Dictionary<string, string>(){{"path", "{root}/build.txt"},{"if_exist", "true"}}), 
 				new BuildNode("show_project_root", "run", new Dictionary<string, string>(){{"path", "ls"}, {"args", "{root}"}}), 
-				new BuildNode("make_build", "run", new Dictionary<string, string>(){{"path", "{root}/run.sh"}, {"work_dir", "{root}"}}),
+				new BuildNode("make_build", "run", new Dictionary<string, string>(){{"path", "{root}/run.sh"}, {"work_dir", "{root}"}, {"log_file", "{root}/log.txt"}}),
 				new BuildNode("validate_build", "check_file_exist", new Dictionary<string, string>{{"path", "{root}/build.txt"}, {"size_more", "1"}}), 
 			};
 			var buildConfig = new Build("test", nodes);
--- Server/Commands/RunCommand.cs
+++ Server/Commands/RunCommand.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Text;
 
 namespace Server.Commands {
 	public class RunCommand:ICommand {
@@ -19,21 +20,51 @@ namespace Server.Commands {
 			args.TryGetValue("args", out commandArgs);
 			string workDir = null;
 			args.TryGetValue("work_dir", out workDir);
+			string logFile = null;
+			args.TryGetValue("log_file", out logFile);
 			try {
 				var startInfo = new ProcessStartInfo(path, commandArgs);
 				if (!string.IsNullOrEmpty(workDir)) {
 					startInfo.WorkingDirectory = workDir;
-				} 
+				}
+				if (!string.IsNullOrEmpty(logFile)) {
+					startInfo.RedirectStandardOutput = true;
+				}
 				var process = new Process {
 					StartInfo = startInfo
 				};
 				process.Start();
-				process.WaitForExit();
-				return CommandResult.Success("");
+				using (var logStream = OpenLogFile(logFile)) {
+					if (logStream != null) {
+						ReadOutputAsync(process.StandardOutput, logStream);
+					}
+					process.WaitForExit();
+				}
+				var resultMessage = "";
+				if (!string.IsNullOrEmpty(logFile)) {
+					resultMessage = $"Log saved to {logFile}.";
+				}
+				return CommandResult.Success(resultMessage);
 			}
 			catch (Exception e) {
 				return CommandResult.Fail($"Failed to run process at \"{path}\": \"{e.ToString()}\"");
 			}
+		}
+
+		FileStream OpenLogFile(string logFile) {
+			if (logFile != null) {
+				return File.OpenWrite(logFile);
+			}
+			return null;
+		}
+		
+		async void ReadOutputAsync(StreamReader reader, FileStream logStream) {
+			string line = await reader.ReadLineAsync();
+			if (!string.IsNullOrEmpty(line)) {
+				var bytes = Encoding.UTF8.GetBytes(line + "\n");
+				logStream.Write(bytes, 0, bytes.Length);
+			}
+			ReadOutputAsync(reader, logStream);
 		}
 	}
 }
